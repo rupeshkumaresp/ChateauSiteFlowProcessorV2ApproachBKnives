8d70d5608dae09436d0d645de84084f9a5ed092d
R.Kumar <r.kumar@espcolour.co.uk>
2021-09-24 12:14:35 +0100

    Prinegy status check

---
+++ UnitTester/PrinergyStatus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChateauSiteFlowApp
+{
+    public class PrinergyStatus
+    {
+        public static bool CheckPrinegyStatus()
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send("192.168.16.231", timeout, buffer, options);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                if (Directory.Exists(@"\\192.168.16.231\AraxiVolume_HW33546-46_J\Jobs\Auto_Impose\SmartHotFolders"))
+                {
+                    return true;
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
--- UnitTester/PushSiteFlowForm.cs
+++ UnitTester/PushSiteFlowForm.cs
@@ -30,32 +30,7 @@ namespace ChateauSiteFlowApp
             this.Close();
         }
 
-        public bool CheckPrinegyStatus()
-        {
-            Ping pingSender = new Ping();
-            PingOptions options = new PingOptions();
-
-            // Use the default Ttl value which is 128,
-            // but change the fragmentation behavior.
-            options.DontFragment = true;
-
-            // Create a buffer of 32 bytes of data to be transmitted.
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            int timeout = 120;
-            PingReply reply = pingSender.Send("192.168.16.231", timeout, buffer, options);
-
-            if (reply.Status == IPStatus.Success)
-            {
-                if (Directory.Exists(@"\\192.168.16.231\AraxiVolume_HW33546-46_J\Jobs\Auto_Impose\SmartHotFolders"))
-                {
-                    return true;
-                }
-            }
-
-
-            return false;
-        }
+      
 
         private void ProcessJsonOrders()
         {
@@ -98,7 +73,7 @@ namespace ChateauSiteFlowApp
                 string path = "";
                 try
                 {
-                    if (!CheckPrinegyStatus())
+                    if (!PrinergyStatus.CheckPrinegyStatus())
                         throw new Exception("PRINERGY SERVER IS NOT ACCESSIBLE");
 
                     OrderHelper orderHelper = new OrderHelper();
